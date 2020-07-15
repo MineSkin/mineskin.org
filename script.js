276a2f488516110e3edb36c94b18e67a2ad46fa5
inventivetalent <mail@inventivetalent.org>
2020-07-16 00:31:03 +0200

    switch version selector to use a select in preparation for supporting 1.16+ UUID change

--- script.js
+++ script.js
@@ -552,7 +552,7 @@ mineskinApp.controller("viewController", ["$scope", "$http", "$cookies", "$timeo
 
     console.log($stateParams);
 
-    $scope.useNewGiveCommand = true;
+    $scope.giveCommandVersion = true;
     $scope.skin = undefined;
     $scope.skinRotation = 35;
     $scope.skinImageLoaded = true;
