dca209abd8a7b0c832fe1157d9f8cd42d4cf1ace
inventivetalent <mail@inventivetalent.org>
2022-03-07 11:53:17 +0100

    .

--- js/controllers/account.script.js
+++ js/controllers/account.script.js
@@ -611,6 +611,11 @@ mineskinApp.controller("accountController", ["$scope", "$http", "$cookies", "$ti
         if (response.data) {
             console.warn(response.data);
             Materialize.toast("Error: " + (response.data.msg || response.data.error));
+            if ((response.data.msg || response.data.error)?.includes("invalid session")) {
+                $timeout(function () {
+                    $scope.logout();
+                }, 1000);
+            }
         } else {
             console.warn(response.data);
         }
