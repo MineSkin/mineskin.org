cb6292db910965be67c55aca9656944207a8acc3
inventivetalent <mail@inventivetalent.org>
2022-03-30 12:30:43 +0200

    .

--- js/controllers/account.script.js
+++ js/controllers/account.script.js
@@ -1,7 +1,19 @@
 mineskinApp.controller("accountController", ["$scope", "$http", "$cookies", "$timeout", "$stateParams", "$sce", "ngMeta", "$window", function ($scope, $http, $cookies, $timeout, $stateParams, $sce, ngMeta, $window) {
     console.info("accountController")
 
-
+    $scope.loadLogin = function () {
+        console.log('loadLogin')
+        google.accounts.id.renderButton(document.getElementById('google_button_placeholder'), {
+            locale: 'en',
+            logo_alignment: 'left',
+            shape: 'pill',
+            size: 'large',
+            text: 'continue_with',
+            theme: 'filled_blue',
+            type: 'standard'
+        });
+        google.accounts.id.prompt();
+    }
 
 
     /// UTIL
