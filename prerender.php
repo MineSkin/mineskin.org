8fcb7d7061766730245d3d8241c7b68074f23825
inventivetalent <mail@inventivetalent.org>
2022-12-04 19:45:49 +0100

    update

--- prerender.php
+++ prerender.php
@@ -1,15 +1,19 @@
 <?php
+// PHP helper script to rewrite crawler requests to the SSR server
+// Change settings in prerender_config.php
+include "./prerender_config.php";
 
-$options = [
-    CURLOPT_RETURNTRANSFER => true,
-];
+header("X-Prerendered: true");
 
-include "prerender_token.php";
-
-$ch = curl_init("https://ssr.inventivetalent.org/render?token=".PRERENDER_TOKEN."&url=".urlencode("https://mineskin.org".$_SERVER["REQUEST_URI"]) );
-curl_setopt_array($ch, $options);
-$remoteSite = curl_exec($ch);
+$ch = curl_init(PRERENDER_HOST . "/render?token=" . PRERENDER_TOKEN . "&url=" . urlencode("https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER["HTTP_USER_AGENT"]);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "X-Prerender-Connecting-IP: " . $_SERVER['HTTP_CF_CONNECTING_IP']
+));
+$res = curl_exec($ch);
 $header = curl_getinfo($ch);
+//header("X-curl-err: ".curl_error($ch)."");
 curl_close($ch);
 
-echo $remoteSite;
+echo $res;
